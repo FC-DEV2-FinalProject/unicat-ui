6eca8f5a506dd43cbeff76d038277972747cac90
yulsanoh <yulsanohdev@gmail.com>
2025-03-20 22:25:57 +0900

    feature: input이 채워지면 로그인 버튼의 색상 변경

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -10,53 +10,68 @@ export default function LoginPage() {
       </div>
       <div className="w-[40%]">
         <div className="w-[350px] flex flex-col items-center justify-center h-full mx-auto">
-          <div className="w-full flex flex-col justify-center h-full mb-[56px]">
-            <h1 className="mb-[36px] text-3xl font-bold self-start">로그인</h1>
-            <div className="flex flex-col gap-4 mb-1">
-              <div>
-                <h2 className="mb-1">이메일</h2>
-                <input
-                  className="w-full border-2 border-gray-100 rounded-md p-2.5 placeholder:text-sm placeholder:text-gray-300 focus:border-[#7879F1] focus:outline-none"
-                  placeholder="이메일을 입력해주세요."
-                  type="email"
-                />
+          <form className="w-full group">
+            <div className="w-full flex flex-col justify-center h-full mb-[56px]">
+              <h1 className="mb-[36px] text-3xl font-bold self-start">
+                로그인
+              </h1>
+              <div className="flex flex-col gap-4 mb-1">
+                <div>
+                  <h2 className="mb-1">이메일</h2>
+                  <input
+                    className="w-full border-2 border-gray-100 rounded-md p-2.5 placeholder:text-sm placeholder:text-gray-300 focus:border-[#7879F1] focus:outline-none"
+                    placeholder="이메일을 입력해주세요."
+                    type="email"
+                    required
+                  />
+                </div>
+                <div>
+                  <h2 className="mb-1">비밀번호</h2>
+                  <input
+                    className="w-full border-2 border-gray-100 rounded-md p-2.5 placeholder:text-sm placeholder:text-gray-300 focus:border-[#7879F1] focus:outline-none"
+                    placeholder="비밀번호를 입력해주세요."
+                    type="password"
+                    required
+                  />
+                </div>
               </div>
-              <div>
-                <h2 className="mb-1">비밀번호</h2>
+              <div className="flex items-center gap-2">
                 <input
-                  className="w-full border-2 border-gray-100 rounded-md p-2.5 placeholder:text-sm placeholder:text-gray-300 focus:border-[#7879F1] focus:outline-none"
-                  placeholder="비밀번호를 입력해주세요."
-                  type="password"
+                  id="KeepLogining"
+                  type="checkbox"
+                  className="w-4 h-4 accent-[#7879F1] cursor-pointer"
                 />
+                <label htmlFor="KeepLogining" className="text-sm text-gray-600">
+                  로그인 유지
+                </label>
               </div>
             </div>
-            <div className="flex items-center gap-2">
-              <input
-                id="KeepLogining"
-                type="checkbox"
-                className="w-4 h-4 accent-[#7879F1] cursor-pointer"
-              />
-              <label htmlFor="KeepLogining" className="text-sm text-gray-600">
-                로그인 유지
-              </label>
+            <div className="flex flex-col items-center gap-4 w-full">
+              <button
+                type="submit"
+                className="flex h-[52px] px-6 py-3 justify-center items-center gap-2.5 self-stretch rounded-lg bg-[#C7C7C7] text-white group-valid:bg-[#7879F1]"
+              >
+                로그인
+              </button>
+              <button
+                type="button"
+                className="flex h-[52px] px-6 py-3 justify-center items-center gap-2.5 self-stretch rounded-lg bg-[#FEE500]"
+              >
+                <img src="/images/KaKao.svg" alt="카카오 로그인 아이콘" />
+                카카오 로그인
+              </button>
+              <button
+                type="button"
+                className="flex h-[52px] px-6 py-3 justify-center items-center gap-2.5 self-stretch rounded-lg bg-[#F2F2F2]"
+              >
+                <img src="/images/Google.svg" alt="구글 로그인 아이콘" />
+                구글 로그인
+              </button>
+              <a className="text-md font-bold text-[#7879F1]" href="#">
+                회원가입
+              </a>
             </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 w-full">
-            <button className="flex h-[52px] px-6 py-3 justify-center items-center gap-2.5 self-stretch rounded-lg bg-[#C7C7C7]">
-              로그인
-            </button>
-            <button className="flex h-[52px] px-6 py-3 justify-center items-center gap-2.5 self-stretch rounded-lg bg-[#FEE500]">
-              <img src="/images/KaKao.svg" alt="카카오 로그인 아이콘" />
-              카카오 로그인
-            </button>
-            <button className="flex h-[52px] px-6 py-3 justify-center items-center gap-2.5 self-stretch rounded-lg bg-[#F2F2F2]">
-              <img src="/images/Google.svg" alt="구글 로그인 아이콘" />
-              구글 로그인
-            </button>
-            <a className="text-md font-bold text-[#7879F1]" href="#">
-              회원가입
-            </a>
-          </div>
+          </form>
         </div>
       </div>
     </div>
