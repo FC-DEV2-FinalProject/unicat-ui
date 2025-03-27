525abf7dea4f9659bdc26e650091790ab2b80d27
whwjdan <35955189+whwjdan@users.noreply.github.com>
2025-03-27 17:06:33 +0900

    feat: msw 재설치
    주요 변경사항:
    - msw 브라우저 및 서버 통합 설정
    - layout에서 msw 정의
    - dev 서버 켜지면 서버 로그 올라옴
    - env에 msw 설정 따라서 온오프

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // webpack: (config, { isServer, dev }) => {
+  //   // 개발 환경에서 MSW 서버 시작
+  //   if (isServer && dev) {
+  //     require('./src/mocks/server.ts');
+  //     console.log('🚀 MSW Server started with Next.js');
+  //   }
+  //   return config;
+  // },
 };
 
 export default nextConfig;
